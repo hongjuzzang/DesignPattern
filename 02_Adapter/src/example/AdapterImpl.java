54e306004a6371ba2e7b95625b4021ca8915ca04
hongju <95hongju@gmail.com>
2020-07-15 20:20:07 +0900

    sec 2 - example1

---
+++ 02_Adapter/src/example/AdapterImpl.java
@@ -0,0 +1,18 @@
+package example;
+
+public class AdapterImpl implements Adapter {
+
+	@Override
+	public Float twiceOf(Float f) {
+		// TODO Auto-generated method stub
+		return (float) Math.twoTime(f.doubleValue());
+	}
+
+	@Override
+	public Float halfOf(Float f) {
+		// TODO Auto-generated method stub
+		return (float) Math.half(f.doubleValue());
+	}
+
+
+}
---
+++ 02_Adapter/src/example/Math.java
@@ -0,0 +1,21 @@
+package example;
+
+public class Math {
+
+	// 이미 구현되어 있는 클래스(요구사항)
+
+	// 두배
+	public static double twoTime(double num) {
+		return num * 2;
+	}
+
+	// 절반
+	public static double half(double num) {
+		return num / 2;
+	}
+
+	// 강화된 알고리즘
+	public static Double doubled(Double d) {
+		return d * 2;
+	}
+}
